c4c2515b51ea84f4966185c9a7b59a1312b10a8d
Pedro Baumann <ondoheer@gmail.com>
2015-06-22 14:06:41 -0500

    fase de ORM, falta que el menu no acepte ni siga en opciones inválidas, hay que hacer que rechace cualquier cosa que no sea Yy

--- flaskb.sh
+++ flaskb.sh
@@ -3,36 +3,8 @@
 #Defining variables and functions
 packagesToInstall[0]="flask" # Set the basic array with packages to install with pip
 
-function menu(){
-    # get options
-    OPTIONS=$1
-    # show options to the user
-   	select opt in $OPTIONS; do
-   		# check if the options are valid
-   		if [[ *"$opt"* != $OPTIONS ]]; then
-   			echo "You selected an invalid choice"
-   			menu $1
 
-   		else
-   			#asign value to global variable
-   			menuresult=$opt
-   		fi
-   	done
-}
 
-function boolMenu(){
-	echo $1 "(Yy/Nn)"
-	read value
-	if [[ "yYnN" != *$value* ]]; then
-		boolMenu $1
-	elif [[ "yY" == *$value* ]]; then
-		return 0
-	else
-		return 1
-	fi
-}
-
-#. ask for name of the project
 echo "#####################"
 echo "##  Flask-Builder  ##"
 echo "#####################"
@@ -46,40 +18,65 @@ mkdir $projectName
 cd $projectName
 
 
-# 2. Choose virtualenv wrapper or virtual env
-
-echo "Quieres Venv?"
-if $(boolMenu); then
-	mkvirtualenvwrapper $projectName
-else
-	virtualenv venv
-fi
+# 2. Choose virtualenv wrapper, virtual env or none
+function virtualenvMenu(){
+	echo "Choose the virtualization method you want to use"
+	OPTIONS="virtualenvwrapper virtualenv None"
+	select opt in $OPTIONS; do
+		if [[ "$opt" = "virtualenvwrapper" ]]; then
+			mkvirtualenv $projectName
+			break
+		elif [[ "$opt" = "virtualenv" ]]; then
+			virtualenv venv
+			break
+		elif [[ "$opt" = "None" ]]; then			
+			echo "You chose None, be sure you run this script as root or that your iser has write permissions to install python packages."
+			break
+		else
+			echo "You chose an invalid option"
+			virtualenvMenu
+		fi
+	done
+}
+virtualenvMenu
 
 # 3. Choose Flask-script or not
 
-echo "Do you want to use Flask-script? I recommedn it!(Yy/Nn)"
+echo "Do you want to use Flask-script? I recommend it!(Yy/Nn)"
 read useFlaskScript
 
 if [[ $useFlaskScript == "y" || useFlaskScript == "Y" ]]; then
-	packagesToInstall=("${packagesToInstall[@]}" "flask-script")
+	packagesToInstall+=" flask-script"
 fi
 
 # 4. Choose the ORM
-
+function ormMenu(){
+	echo "Choose an ORM library"
+	OPTIONS="flask-sqlalchemy pony flask-mongoengine"
+	select opt in $OPTIONS; do
+		packagesToInstall+=" "
+		packagesToInstall+=$opt
+		break
+	
+			
+	done
+}
 echo "Do you want to initialize an ORM?(Yy/Nn)"
-read userORM
+read useORM
 if [[ $useORM == "y" || $useORM == "Y" ]]; then
-	echo "Choose one of the following"
-	echo "1. Flask-SQLAlchemy"
-	echo "2. PonyORM"
-	echo "3. Mongoengine"
-	echo "choose one: (1, 2, 3)"
+	ormMenu
+	echo "using ormmenu"
 fi
 
+echo $packagesToInstall
+
+
+# FIN TEMPORAL DE LIMPIEZA
+cd ..
+rm -rf $projectName
 
 #. ask for flask-script
 #. ask for ORM to use(Yy/Nn)
 #. ask for packages to use
 #. ask for heroku
 #. sass or css
-
